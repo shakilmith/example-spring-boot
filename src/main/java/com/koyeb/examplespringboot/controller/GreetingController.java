cb309ace25818efb270689a0d30ce09e97acbf82
shakilmith <shakilmith2@gmail.com>
2024-11-22 00:21:47 +0600

    new commit

---
+++ src/main/java/com/koyeb/examplespringboot/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.koyeb.examplespringboot.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hi")
+    public String getGreeting(){
+        return "Hello, World";
+    }
+}
